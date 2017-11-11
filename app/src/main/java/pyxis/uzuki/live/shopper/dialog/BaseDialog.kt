0af7a75efec4db32d4b9aa271a3d1ab4f1b0e6f4
WindSekirun <h.mina.moe@gmail.com>
2017-11-11 22:37:31 +0900

    remove unused dependencies

--- app/src/main/java/pyxis/uzuki/live/shopper/dialog/BaseDialog.kt
+++ app/src/main/java/pyxis/uzuki/live/shopper/dialog/BaseDialog.kt
@@ -4,7 +4,6 @@ import android.app.Dialog
 import android.content.Context
 import android.view.View
 
-import pyxis.uzuki.live.pyxinjector.PyxInjector
 import pyxis.uzuki.live.shopper.R
 
 /**
@@ -16,15 +15,8 @@ import pyxis.uzuki.live.shopper.R
  * Description:
  */
 open class BaseDialog : Dialog {
-    protected var injector = PyxInjector()
+    constructor(context: Context) : super(context, R.style.AppThemeDialog)
 
-    constructor(context: Context) : super(context, R.style.AppThemeDialog) {}
-
-    constructor(context: Context, themeResId: Int) : super(context, R.style.AppThemeDialog) {}
-
-    override fun onContentChanged() {
-        super.onContentChanged()
-        injector.execute(context, this, findViewById<View>(android.R.id.content))
-    }
+    constructor(context: Context, themeResId: Int) : super(context, R.style.AppThemeDialog)
 
 }
